02cd8c5fd61a5099bb502d5c53e799485fe6c2b2
John Snow <thelunaticscripter@outlook.com>
2017-04-12 13:23:21 -0400

    Install pre-req software for garcon-de-chef-training
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- metadata.rb
+++ metadata.rb
@@ -11,10 +11,10 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/cookbook-garcon-de-chef-training/issues'
+issues_url 'https://github.com/TheLunaticScripter/cookbook-garcon-de-chef-training/issues'
 
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/cookbook-garcon-de-chef-training'
+source_url 'https://github.com/TheLunaticScripter/cookbook-garcon-de-chef-training'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Install Pre-Req Software
+
+package 'unzip'
+
+remote_file '/home/ubuntu/terraform.zip' do
+  source node['cookbook-garcon-de-chef-training']['terraform_url']
+  owner 'root'
+  group 'root'
+  checksum node['cookbook-garcon-de-chef-training']['terraform_checksum']
+end
+
+execute 'unzip_terraform' do
+  command 'unzip -q -o /home/ubuntu/terraform.zip -d /usr/local/bin'
+  action :run
+  not_if { File.exist?('/usr/local/bin/terraform') }
+end
+
+package 'ruby'
